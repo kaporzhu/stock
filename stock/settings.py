0891d4d4aff878920e611f13d6627be16645aa27
Kapor <smokingkapor@gmail.com>
2015-01-31 21:43:31 +0800

    Added STATIC_ROOT settings

--- stock/settings.py
+++ stock/settings.py
@@ -96,6 +96,7 @@ SERVER_EMAIL = 'kapor.zhu@gmail.com'
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 try:
     from local_settings import *  # noqa
