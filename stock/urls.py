879b0631324c51b8e973d22cf1ee760ec337d147
Kapor <smokingkapor@gmail.com>
2015-02-02 12:19:58 +0800

    Redirect home page to /companies/

--- stock/urls.py
+++ stock/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(permanent=False, url='/companies/')),
     url(r'^companies/', include('companies.urls', 'companies', 'companies')),
 
     url(r'^admin/', include(admin.site.urls)),
