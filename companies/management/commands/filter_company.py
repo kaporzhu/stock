f01a511cd3c28d4443a1a19d69ce8fcdf824eb40
Kapor <smokingkapor@gmail.com>
2015-02-12 16:07:50 +0800

    Fixed bug in filter_company command

--- companies/management/commands/filter_company.py
+++ companies/management/commands/filter_company.py
@@ -9,14 +9,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         SelectedCompany.objects.all().delete()
         for company in Company.objects.all():
-            initial_price = final_price =None
+            latest_price = oldest_price =None
             good = True
             for stock in company.stock_set.order_by('-created_at')[:5]:
                 if stock.pricechange < 0:
                     good = False
                     break
-                if not initial_price:
-                    initial_price = stock.trade
-                final_price = stock.trade
-            if good and initial_price and (final_price-initial_price)/initial_price >= 0.1:
+                if not latest_price:
+                    latest_price = stock.trade
+                oldest_price = stock.trade
+            if good and latest_price and oldest_price and (latest_price-oldest_price)/oldest_price >= 0.1:
                 SelectedCompany(company=company).save()
